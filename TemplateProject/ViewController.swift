76f89d46584eb3ae3e4d67fa2d0e0d01522cb1a1
andrey <andrew@wearemad.ru>
2019-04-10 17:44:03 +0400

    Initial Commit

---
+++ TemplateProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TemplateProject
+//
+//  Created by Андрей Зубехин on 10/04/2019.
+//  Copyright © 2019 MAD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
